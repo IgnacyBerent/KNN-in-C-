f95fd512781c60d09300f6dcfdbe16af583b54bb
IgnacyBerent <2gb02ignac@gmail.com>
2023-12-04 21:49:37 +0100

    Implementing class in the correct way, small optimization

--- library.cpp
+++ library.cpp
@@ -4,88 +4,91 @@
 #include <vector>
 #include <algorithm>
 
-class KNN {
-    std::vector<int> centroids;
-    std::vector<float> list;
-    int n;
+// constructor
+KNN::KNN(std::vector<float> data, int neighbours) {
+    this->data = data;
+    std::sort(this->data.begin(), this->data.end());
+    this->n = neighbours;
+    create_centroids();
+    train();
+}
 
-public:
-    // constructor
-    KNN(std::vector<float> list, int neighbours) {
-        this->list = list;
-        std::sort(this->list.begin(), this->list.end());
-        this->n = neighbours;
-        this->centroids = create_centroids();
-        predict();
+// predict the closest centroid to the given number
+int KNN::predict(float number) {
+    int closest = closest_centroid(number);
+    return centroids[closest];
+}
+
+// create random centroids
+void KNN::create_centroids() {
+    for (int i = 0; i < n; i++) {
+        int random = rand() % 100;
+        centroids.push_back(random);
     }
+}
 
-private:
-    // create random centroids
-    std::vector<int> create_centroids() {
-        for (int i = 0; i < n; i++) {
-            int random = rand() % 100;
-            centroids.push_back(random);
+// calculated closest centroid to the given number
+int KNN::closest_centroid(int number) {
+    int closest = 0;
+    int distance = 0;
+    for (int i = 0; i < centroids.size(); i++) {
+        int temp = abs(centroids[i] - number);
+        if (distance == 0) {
+            distance = temp;
+            closest = i;
         }
-        return centroids;
-    }
-
-    // calculated closesd centroid to the given number
-    int closest_centroid(int number) {
-        int closest = 0;
-        int distance = 0;
-        for (int i = 0; i < centroids.size(); i++) {
-            int temp = abs(centroids[i] - number);
-            if (distance == 0) {
-                distance = temp;
-                closest = i;
-            }
-            else if (temp < distance) {
-                distance = temp;
-                closest = i;
-            }
+        else if (temp < distance) {
+            distance = temp;
+            closest = i;
         }
-        return closest;
     }
+    return closest;
+}
 
-    // find closest centroid to each number in the list
-    std::vector<int> find_closest_centroids() {
-        std::vector<int> closest_centroids;
-        for (int i = 0; i < this->list.size(); i++) {
-            closest_centroids.push_back(closest_centroid(this->list[i]));
-        }
-        return closest_centroids;
+// find a closest centroid to each number in the list
+std::vector<int> KNN::find_closest_centroids() {
+    std::vector<int> closest_centroids;
+    for (int i = 0; i < this->data.size(); i++) {
+        closest_centroids.push_back(closest_centroid(this->data[i]));
     }
+    return closest_centroids;
+}
 
-    // calculate mean of each centroid
-    void calculate_mean(std::vector<int> closest_centroids) {
-        std::vector<int> means;
-        for (int i = 0; i < centroids.size(); i++) {
-            int sum = 0;
-            int count = 0;
-            for (int j = 0; j < closest_centroids.size(); j++) {
-                if (closest_centroids[j] == i) {
-                    sum += this->list[j];
-                    count++;
-                }
-            }
-            if (count != 0) {
-                means.push_back(sum/count);
-            } else {
-                means.push_back(0);
+// calculate mean of each centroid
+void KNN::calculate_mean(std::vector<int>& closest_centroids) {
+    std::vector<int> means;
+    for (int i = 0; i < centroids.size(); i++) {
+        int sum = 0;
+        int count = 0;
+        for (int j = 0; j < closest_centroids.size(); j++) {
+            if (closest_centroids[j] == i) {
+                sum += this->data[j];
+                count++;
             }
         }
-        this->centroids = means;
+        if (count != 0) {
+            means.push_back(sum/count);
+        } else {
+            means.push_back(0);
+        }
     }
+    this->centroids = means;
+}
 
-    // predict centroids until they stop changing
-    void predict() {
-        std::vector<int> closest_centroids = find_closest_centroids();
+// train model until centroids stop changing
+void KNN::train() {
+    std::vector<int> closest_centroids = find_closest_centroids();
+    calculate_mean(closest_centroids);
+    while (true) {
+        std::vector<int> old_centroids = closest_centroids;
+        closest_centroids = find_closest_centroids();
+        if (old_centroids == closest_centroids) {
+            break;
+        }
         calculate_mean(closest_centroids);
-        std::vector<int> new_closest_centroids = find_closest_centroids();
-        while (closest_centroids != new_closest_centroids) {
-            closest_centroids = new_closest_centroids;
-            calculate_mean(closest_centroids);
-            new_closest_centroids = find_closest_centroids();
-        }
     }
-};
+
+    // Clear and shrink the data vector after training
+    data.clear();
+    data.shrink_to_fit();
+}
--- library.h
+++ library.h
@@ -1,6 +1,23 @@
-#ifndef UNTITLED_LIBRARY_H
-#define UNTITLED_LIBRARY_H
+#ifndef LIBRARY_H
+#define LIBRARY_H
 
-void hello();
+#include <vector>
 
-#endif //UNTITLED_LIBRARY_H
+class KNN {
+public:
+    KNN(std::vector<float> data, int neighbours);
+    int predict(float number);
+
+private:
+    void create_centroids();
+    int closest_centroid(int number);
+    std::vector<int> find_closest_centroids();
+    void calculate_mean(std::vector<int>& closest_centroids);
+    void train();
+
+    std::vector<int> centroids;
+    std::vector<float> data;
+    int n;
+};
+
+#endif // LIBRARY_H
